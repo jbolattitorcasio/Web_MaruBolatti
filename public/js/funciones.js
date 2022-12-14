e19b2dbf7e976aa7dd677af797ba32818b363f79
jbolattitorcasio <jbolattitorcasio@frba.utn.edu.ar>
2022-12-14 00:31:34 -0300

    Home and About structure, css, and lottie implementation

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,30 +1,37 @@
 const localhost = "http://localhost:3000/"; //Borrar para la produccion
 const host = "https://marubolatti.com/";
 
+// NAVBAR
 const nav = document.querySelector('.nav-main');
 const siteLogo = document.querySelector('.site-logo img');
-const navMedia = document.querySelector('.nav-media');
-const navBackground = document.querySelector('.background');
+const navContainer  = document.querySelector('.nav-container');
+const navMain = document.querySelector('.nav-main');
+
 function opener() {
     nav.classList.toggle('is-open'); 
     if(nav.classList.contains('is-open')) {
-        siteLogo.src = "/img/logo-bw.png";
-        navMedia.style.display = "flex";
-        navBackground.classList.remove('background-animation-reverse');
-        navBackground.classList.add('background-animation');
+        if((window.location.href == host + 'produccion') || (window.location.href == localhost + 'produccion')) {
+            siteLogo.src = "/img/logo-prod.png";
+        }
+        if((window.location.href == host + 'direccion') || (window.location.href == localhost + 'direccion')) {
+            siteLogo.src = "/img/logo-direc.png";
+        }
+        if((window.location.href == host + 'contacto') || (window.location.href == localhost + 'contacto')) {
+            siteLogo.src = "/img/logo-bw.png";
+        }
+        navContainer.style.display = "block";
     } else {
         siteLogo.src = "/img/logo.png";
-        navMedia.style.display = "none";
-        navBackground.classList.remove('background-animation');
-        navBackground.classList.add('background-animation-reverse');
+        navContainer.style.display = "none";
     }
 }
 
+// PROD and DIREC Configuration
 if((window.location.href == host + 'produccion') || (window.location.href == localhost + 'produccion')) {
-    navBackground.style.background = "var(--color-main-pink)";
+    navMain.style.background = "var(--color-main-pink)";
 }
 if((window.location.href == host + 'direccion') || (window.location.href == localhost + 'direccion')) {
-    navBackground.style.background = "var(--color-main-blue)";
+    navMain.style.background = "var(--color-main-blue)";
 }
 
 // FOOTER
@@ -34,56 +41,43 @@ if((window.location.href == host) || (window.location.href == localhost)) {
 } else {
     footer.style.position = 'static';
 }
-/*
-//SWIPER
-import Swiper, { Navigation, Pagination } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+// AOS Configuration
+AOS.init();
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 });
-swiper.slideNext();
-*/
 
-// // AOS Configuration
-// AOS.init();
-// AOS.init({
-//   // Global settings:
-//   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-//   startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-//   initClassName: 'aos-init', // class applied after initialization
-//   animatedClassName: 'aos-animate', // class applied on animation
-//   useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-//   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-//   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-//   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-//   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-//   offset: 120, // offset (in px) from the original trigger point
-//   delay: 0, // values from 0 to 3000, with step 50ms
-//   duration: 400, // values from 0 to 3000, with step 50ms
-//   easing: 'ease', // default easing for AOS animations
-//   once: false, // whether animation should happen only once - while scrolling down
-//   mirror: false, // whether elements should animate out while scrolling past them
-//   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-// });
-
+// Animation Lottie 
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animAbout01'),
+    rederer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/js/animations/T_01.json'
+});
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animAbout02'),
+    rederer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '/js/animations/T_02.json'
+});
